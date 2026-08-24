30fec4abb2959f6d6cbe21d58b76410871d46c63
XurxoMF <xurxolaxe@gmail.com>
2026-08-24 23:54:34 +0200

    Fixed: Small bug introduced on the latest comit on the Sidebar.Inset

--- src/lib/components/ui/sidebar/sidebar-inset.svelte
+++ src/lib/components/ui/sidebar/sidebar-inset.svelte
@@ -16,7 +16,7 @@
 	bind:this={ref}
 	data-slot="sidebar-inset"
 	class={cn(
-		"@container/main relative flex min-h-0 w-full flex-1 flex-col bg-background md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
+		"@container/main relative flex min-h-0 w-full flex-1 flex-col bg-background md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2",
 		className
 	)}
 	{...restProps}
